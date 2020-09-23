2123d44e0f9c4ed7941272c341487feee989f5e2
casio0128-dev <sweet.cafe0141@gmail.com>
2020-09-23 13:11:30 +0900

    edit App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App uk-container uk-background-muted">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React2
-        </a>
-      </header>
+    <div className="App uk-container uk-background-muted uk-container-expand">
+      <h1>Hello,World!</h1>
     </div>
   );
 }
